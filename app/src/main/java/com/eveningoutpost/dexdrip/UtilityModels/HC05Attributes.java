8cefba31061dbf7f160dbdb3481b14d4bfe019de
raduiliescu <radu.iliescu@yahoo.com>
2015-01-26 08:58:35 +0200

    utility: add model for HC05 bluetooth (non BLE)

---
+++ app/src/main/java/com/eveningoutpost/dexdrip/UtilityModels/HC05Attributes.java
@@ -0,0 +1,8 @@
+package com.eveningoutpost.dexdrip.UtilityModels;
+
+/**
+ * Created by Radu Iliescu on 1/24/2015.
+ */
+public class HC05Attributes {
+    public static String UUIDString = "00001101-0000-1000-8000-00805F9B34FB";
+}
